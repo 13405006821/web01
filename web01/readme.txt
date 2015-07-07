4d8509888861b19a5fa989868c1d421c66e915ad
liupeng <13405006821@139.com>
2015-07-07 22:41:23 +0800

    in

--- web01/readme.txt
+++ web01/readme.txt
@@ -2,3 +2,4 @@
 2.事务管理
 3.pushlet
 4.ajax html
+5.加入ehcache
--- web01/src/test/java/com/test/WebServiceTest.java
+++ web01/src/test/java/com/test/WebServiceTest.java
@@ -2,23 +2,36 @@ package com.test;
 
 import javax.annotation.Resource;
 
+import net.sf.ehcache.Cache;
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.Element;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.web.service.IFunctionService;
-
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={"classpath:spring-hibernate.xml", "classpath:spring.xml"})
-public class WebServiceTest extends AbstractJUnit4SpringContextTests{
-
+@ContextConfiguration(locations = { "classpath:spring-hibernate.xml", "classpath:spring.xml" })
+public class WebServiceTest extends AbstractJUnit4SpringContextTests {
 	@Resource
-	private IFunctionService functionService;
-	
+	private CacheManager cacheManager;
+
 	@Test
-	public void test(){
-		System.out.println("===123456");
+	public void test() {
+		Cache testCache = cacheManager.getCache("testCache");
+		Element element = testCache.get("key");
+		for (int i = 0; i < 10; i++) {
+			if (element == null) {
+				element = new Element("key", "asdadadaddadadad");
+				testCache.put(element);
+				System.out.println("无法从缓存中取到");
+			} else {
+				String val = (String) element.getValue();
+				System.out.println(val);
+				System.out.println("从缓存中取到");
+			}
+		}
 	}
 }
