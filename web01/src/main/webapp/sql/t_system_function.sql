c7de109a9baaad6ad195ad388c87eed8972f1e42
liupeng <13405006821@139.com>
2015-07-08 10:32:38 +0800

    up

--- web01/src/main/webapp/sql/t_system_function.sql
+++ web01/src/main/webapp/sql/t_system_function.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50538
 File Encoding         : 65001
 
-Date: 2015-07-07 17:14:20
+Date: 2015-07-08 10:32:20
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -29,7 +29,7 @@ CREATE TABLE `t_system_function` (
   `url` varchar(255) DEFAULT NULL,
   `important` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of t_system_function
@@ -44,3 +44,4 @@ INSERT INTO `t_system_function` VALUES ('13', '1', '2015-06-11 14:38:34', '2015-
 INSERT INTO `t_system_function` VALUES ('14', '0', null, '2015-06-09 23:16:00', '项目加入自定义注解', '9', null, '5');
 INSERT INTO `t_system_function` VALUES ('16', '1', '2015-06-10 07:44:03', '2015-06-10 07:43:58', '功能信息管理(ajax-html + ymPrompt + 实现分页功能jsptags)', '3', 'systemFunction.action', '10');
 INSERT INTO `t_system_function` VALUES ('20', '0', null, '2015-06-11 14:54:03', 'http和https间全部和部分转换', '10', 'page_https.action?result=https', '5');
+INSERT INTO `t_system_function` VALUES ('21', '0', null, '2015-07-08 10:28:12', 'ehcache使用', '11', 'cache.action', '5');
